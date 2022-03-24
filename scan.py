509fa1c01f5e96786d23095c706e63f7aaced051
Adon Metcalfe <adonm@fastmail.fm>
2022-03-24 23:59:01 +0800

    add save step at end

--- scan.py
+++ scan.py
@@ -29,38 +29,7 @@ header_args = [
 rumble_sites_file = cwd / "outputs" / "rumble_sites.json"
 run(["curl", "-o", rumble_sites_file, *header_args, f"{api}/org/sites"])
 # All services export
-run(
-    [
-        "curl",
-        "-o",
-        cwd / "outputs" / "rumble_services.json",
-        *header_args,
-        f"{api}/export/org/services.json",
-    ]
-)
-ips = {}
-for row in json.load((cwd / "outputs" / "rumble_services.json").open()):
-    if row["site_name"] not in ips:
-        ips[row["site_name"]] = set()
-    for ip in row["addresses"]:
-        ips[row["site_name"]].add(ip)
-json.dump(ips, (cwd / "outputs" / "rumble_ips.json").open("w"), default=list)
-# Potentially insecure services export
-search = """alive:t AND (not transport:icmp)
-AND ((protocol:"tls" AND (tls.supportedVersionNames:"SSL" OR tls.supportedVersionNames:"TLSv1.0" OR tls.supportedVersionNames:"TLSv1.1")) OR (not protocol:tls))
-AND ((protocol:http AND (http.code:<=300 OR http.code:>=400)) OR (not protocol:http))
-"""
-run(
-    [
-        "curl",
-        "-o",
-        cwd / "outputs" / "rumble_services_insecure.json",
-        *header_args,
-        "--data-urlencode",
-        f"search={search}",
-        f"{api}/export/org/services.json",
-    ]
-)
+run(["curl", "-o", cwd / "outputs" / "rumble_services.json", *header_args, f"{api}/export/org/services.json"])
 
 
 rumble_sites = {}
@@ -104,3 +73,6 @@ for site in cwd.glob("inputs/*.domains.txt"):
         url = f"{api}/org/sites"
         print(f"Creating new site for {sitename} at {url}")
         run(["curl", *header_args, "-d", data, "-X", "PUT", url])
+
+run(["az", "storage", "blob", "upload-batch", "-d", os.environ["BLOB_CONTAINER"], "-s", cwd.parent, "--overwrite", "true"
+    "--connection-string", os.environ["AZURE_STORAGE_CONNECTION_STRING"], "--pattern",  "rumble/*"])
