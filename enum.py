8c7dea4bcf41aa9a120cade08e8e7e033e6e6d35
Adon Metcalfe <adonm@fastmail.fm>
2022-03-24 22:52:11 +0800

    undo caching in prep for parallel tasks

--- enum.py
+++ enum.py
@@ -19,46 +19,40 @@ header_args = [
 ]
 
 rumble_sites_file = cwd / "outputs" / "rumble_sites.json"
-if (
-    rumble_sites_file.exists()
-    and time() - rumble_sites_file.stat().st_mtime < 60 * 60 * 24
-):
-    print(f"Skipping rumble exports as updated in last 24 hours")
-else:
-    run(["curl", "-o", rumble_sites_file, *header_args, f"{api}/org/sites"])
-    # All services export
-    run(
-        [
-            "curl",
-            "-o",
-            cwd / "outputs" / "rumble_services.json",
-            *header_args,
-            f"{api}/export/org/services.json",
-        ]
-    )
-    ips = {}
-    for row in json.load((cwd / "outputs" / "rumble_services.json").open()):
-        if row["site_name"] not in ips:
-            ips[row["site_name"]] = set()
-        for ip in row["addresses"]:
-            ips[row["site_name"]].add(ip)
-    json.dump(ips, (cwd / "outputs" / "rumble_ips.json").open("w"), default=list)
-    # Potentially insecure services export
-    search = """alive:t AND (not transport:icmp)
-    AND ((protocol:"tls" AND (tls.supportedVersionNames:"SSL" OR tls.supportedVersionNames:"TLSv1.0" OR tls.supportedVersionNames:"TLSv1.1")) OR (not protocol:tls))
-    AND ((protocol:http AND (http.code:<=300 OR http.code:>=400)) OR (not protocol:http))
-    """
-    run(
-        [
-            "curl",
-            "-o",
-            cwd / "outputs" / "rumble_services_insecure.json",
-            *header_args,
-            "--data-urlencode",
-            f"search={search}",
-            f"{api}/export/org/services.json",
-        ]
-    )
+run(["curl", "-o", rumble_sites_file, *header_args, f"{api}/org/sites"])
+# All services export
+run(
+    [
+        "curl",
+        "-o",
+        cwd / "outputs" / "rumble_services.json",
+        *header_args,
+        f"{api}/export/org/services.json",
+    ]
+)
+ips = {}
+for row in json.load((cwd / "outputs" / "rumble_services.json").open()):
+    if row["site_name"] not in ips:
+        ips[row["site_name"]] = set()
+    for ip in row["addresses"]:
+        ips[row["site_name"]].add(ip)
+json.dump(ips, (cwd / "outputs" / "rumble_ips.json").open("w"), default=list)
+# Potentially insecure services export
+search = """alive:t AND (not transport:icmp)
+AND ((protocol:"tls" AND (tls.supportedVersionNames:"SSL" OR tls.supportedVersionNames:"TLSv1.0" OR tls.supportedVersionNames:"TLSv1.1")) OR (not protocol:tls))
+AND ((protocol:http AND (http.code:<=300 OR http.code:>=400)) OR (not protocol:http))
+"""
+run(
+    [
+        "curl",
+        "-o",
+        cwd / "outputs" / "rumble_services_insecure.json",
+        *header_args,
+        "--data-urlencode",
+        f"search={search}",
+        f"{api}/export/org/services.json",
+    ]
+)
 
 
 rumble_sites = {}
@@ -69,30 +63,23 @@ for site in json.loads(rumble_sites_file.open().read()):
 for site in cwd.glob("inputs/*.domains.txt"):
     name = site.name.split(".domains")[0]
     subdomains = cwd / "outputs" / f"{name}.subdomains.txt"
-    if (
-        subdomains.exists()
-        and time() - subdomains.stat().st_mtime < 60 * 60 * 48
-        and site.stat().st_mtime < subdomains.stat().st_mtime
-    ):
-        print(f"Skipping {name} as updated in last 48 hours")
-    else:
-        print(f"Enumerating {name} subdomains...")
-        new = subdomains.with_suffix(".new")
-        cmd = [
-            "amass",
-            "enum",
-            "-active",
-            "-df",
-            site,
-            "-dir",
-            cwd / "amassdb",
-            "-o",
-            new,
-        ]
-        if subdomains.exists():
-            cmd += ["-nf", subdomains]
-        run(cmd)
-        new.replace(subdomains)
+    print(f"Enumerating {name} subdomains...")
+    new = subdomains.with_suffix(".new")
+    cmd = ["docker", "run", "-v", f"{cwd}:{cwd}", "caffix/amass"
+        "amass",
+        "enum",
+        "-active",
+        "-df",
+        site,
+        "-dir",
+        cwd / "amassdb",
+        "-o",
+        new,
+    ]
+    if subdomains.exists():
+        cmd += ["-nf", subdomains]
+    run(cmd)
+    new.replace(subdomains)
     data = json.dumps(
         {
             "name": name.upper(),
